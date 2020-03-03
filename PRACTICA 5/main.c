12c7ac770ccc46bef90cff0898cf7aaa0e6b6cac
MISAELALFA <60374144+MISAELALFA@users.noreply.github.com>
2020-03-02 21:17:20 -0600

    SENTENCIAS CON WHILE Y DO WHILE

---
+++ PRACTICA 5/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int N=0, sumpos=0, n=0;
+float prompos=0;
+
+int main(int argc, char *argv[]) {
+	printf("Programa para promedio de numeros positivos.\n");
+	printf("Ingrese el primer numero: ");
+	scanf("%d", &N);
+ 	int i = 1;
+ 	while(N>0){
+ 		printf("Ingrese el numero %d: \t", i+1);
+		sumpos=sumpos+N;
+		scanf("%d", &N);
+ 		i++;
+	
+	}
+	n=i-1;
+	printf("FIN\n");
+	prompos=sumpos/n;
+	printf("El promedio de numeros positivos es: %f", prompos);
+	return 0;
+	}
